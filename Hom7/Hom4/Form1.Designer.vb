0d4652a00820c91e2541b2b63c7ee349496285d9
Tommolo <72791771+Tommolo@users.noreply.github.com>
2022-11-19 10:19:04 +0100

    Hw7

---
+++ Hom7/Hom4/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TrackBar3 = New System.Windows.Forms.TrackBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(636, 101)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(142, 111)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Compute"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(34, 39)
+        Me.TrackBar1.Maximum = 50
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(130, 56)
+        Me.TrackBar1.TabIndex = 3
+        Me.TrackBar1.Value = 20
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(65, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 20)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Trials: 20"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(34, 101)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(377, 289)
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(641, 238)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(137, 152)
+        Me.RichTextBox1.TabIndex = 8
+        Me.RichTextBox1.Text = ""
+        '
+        'TrackBar3
+        '
+        Me.TrackBar3.Location = New System.Drawing.Point(236, 39)
+        Me.TrackBar3.Maximum = 40
+        Me.TrackBar3.Name = "TrackBar3"
+        Me.TrackBar3.Size = New System.Drawing.Size(130, 56)
+        Me.TrackBar3.TabIndex = 9
+        Me.TrackBar3.Value = 10
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(269, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Lambda: 10"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(431, 100)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(199, 290)
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TrackBar3)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents TrackBar3 As TrackBar
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ Hom7/Hom4/Form1.vb
@@ -0,0 +1,135 @@
+﻿Public Class Form1
+
+    Private Bitmap As Bitmap
+    Private Graphics As Graphics
+    Private Bitmap2 As Bitmap
+    Private Graphics2 As Graphics
+    Private rand As Random = New Random()
+    Private PenAbso As Pen = New Pen(Color.LightGreen, 2)
+    Dim interarrivals As List(Of Integer)
+    Dim interarrivals_distribution_dictionary As SortedDictionary(Of Integer, Integer)
+
+
+    Private Function tranX(ByVal x As Double, ByVal minX As Double, ByVal maxX As Double, ByVal w As Integer) As Integer
+        Return CInt((w * (x - minX) / (maxX - minX)))
+    End Function
+
+    Private Function tranY(ByVal y As Double, ByVal minY As Double, ByVal maxY As Double, ByVal h As Integer) As Integer
+        Return CInt((h - h * (y - minY) / (maxY - minY)))
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Bitmap = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        Graphics = Graphics.FromImage(Bitmap)
+        Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality
+        Graphics.Clear(Color.Black)
+        Dim trials = TrackBar1.Value
+        Dim lambda = TrackBar3.Value
+        interarrivals = New List(Of Integer)
+        Dim count_interarrivals = 0
+        Dim rectangle = New Rectangle(20, 20, Bitmap.Width, Bitmap.Height)
+        'Graphics.FillRectangle(Brushes.Black, rectangle)
+        'Graphics.DrawRectangle(Pens.Black, rectangle)
+        RichTextBox1.Clear()
+
+        Dim probability As Double = (lambda / trials) * 100
+        RichTextBox1.AppendText("probability: " & probability)
+        RichTextBox1.AppendText(Environment.NewLine)
+
+
+        Dim minX_a As Double = 0
+        Dim maxX_a As Double = CDbl(trials)
+        Dim minY_a As Double = 0
+        Dim maxY_a As Double = CDbl(trials)
+
+
+        Dim y_a As Double = 0
+        Dim points_a As List(Of Point) = New List(Of Point)
+        For tria_num As Integer = 0 To trials - 1
+            Dim random_number = rand.Next(0, 100)
+            If random_number <= probability Then
+                y_a = y_a + 1
+                interarrivals.Add(count_interarrivals)
+                count_interarrivals = 0
+            Else
+                count_interarrivals = count_interarrivals + 1
+            End If
+            'Absolute
+            Dim cord_x_a As Integer = tranX(tria_num, minX_a, maxX_a, rectangle.Width)
+            Dim cord_y_a As Integer = tranY(y_a, minY_a, maxY_a, rectangle.Height)
+            Dim p_a As Point = New Point(cord_x_a, cord_y_a)
+            points_a.Add(p_a)
+            'RichTextBox1.AppendText(tria_num.ToString & ": " & y_a)
+            'RichTextBox1.AppendText(Environment.NewLine)
+        Next
+        Graphics.DrawLines(PenAbso, points_a.ToArray)
+        PictureBox1.Image = Bitmap
+        interarrivals_distribution()
+        draw_interarrivals_distribuiton()
+    End Sub
+
+    Private Sub interarrivals_distribution()
+        interarrivals_distribution_dictionary = New SortedDictionary(Of Integer, Integer)
+        For Each elem In interarrivals
+            'RichTextBox1.AppendText("elem: " & elem)
+            'RichTextBox1.AppendText(Environment.NewLine)
+            If Not interarrivals_distribution_dictionary.ContainsKey(elem) Then
+                interarrivals_distribution_dictionary.Add(elem, 1)
+
+            Else
+                interarrivals_distribution_dictionary(elem) += 1
+            End If
+            'RichTextBox1.AppendText("elem_val: " & interarrivals_distribution_dictionary(elem))
+            'RichTextBox1.AppendText(Environment.NewLine)
+        Next
+    End Sub
+
+    Private Sub draw_interarrivals_distribuiton()
+        Bitmap2 = New Bitmap(PictureBox2.Width, PictureBox2.Height)
+        Graphics2 = Graphics.FromImage(Bitmap2)
+        Graphics2.Clear(Color.Black)
+        Dim rectangle2 = New Rectangle(0, 0, Bitmap2.Width, Bitmap2.Height)
+
+        Dim max_value = interarrivals_distribution_dictionary.Values.Max
+        Dim space_height = rectangle2.Bottom - rectangle2.Top
+        Dim space_width = rectangle2.Right - rectangle2.Left
+        Dim number_of_keys = interarrivals_distribution_dictionary.Keys.Count
+        Dim bar_width = space_width / number_of_keys
+
+        Dim start = rectangle2.X
+
+        For Each key In interarrivals_distribution_dictionary
+            Dim rect_height = key.Value / max_value * space_height
+            Dim bar_rect = New Rectangle(start, rectangle2.Bottom - rect_height, bar_width, rect_height)
+
+            Graphics2.FillRectangle(Brushes.Green, bar_rect)
+            Graphics2.DrawRectangle(Pens.White, bar_rect)
+            start += bar_width
+
+            RichTextBox1.AppendText("key: " & key.Key)
+            RichTextBox1.AppendText(Environment.NewLine)
+            RichTextBox1.AppendText("value: " & key.Value)
+            RichTextBox1.AppendText(Environment.NewLine)
+        Next
+
+
+
+
+        PictureBox2.Image = Bitmap2
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        Label1.Text = "Trials: " & TrackBar1.Value.ToString
+    End Sub
+
+
+    Private Sub TrackBar3_Scroll(sender As Object, e As EventArgs) Handles TrackBar3.Scroll
+        Label3.Text = "Lambda: " & TrackBar3.Value.ToString
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
